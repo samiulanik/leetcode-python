4cb6ba4051c8178bc3920e7b9bb7d93da782893f
Samiul Anik <samiul.anik@gmail.com>
2022-04-17 16:27:23 -0700

    first test added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This repository is for solving Leetcode problems using python 3.
+
+The `tests` folder contains the test cases provided in the leetcode.
+
+GitHub Actions is enabled with pytest and pre-commit hook
---
+++ solutions/two_pointer/three_sum.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/3sum/
+from typing import List
+
+
+def threeSum(nums: List[int]) -> List[List[int]]:
+    result, size = [], len(nums)
+    if size < 3:
+        return result
+
+    nums.sort()
+    for i in range(size):
+        if i - 1 >= 0 and nums[i] == nums[i - 1]:
+            continue
+        left, right = i + 1, size - 1
+        while left < right:
+            target = nums[i] + nums[left] + nums[right]
+            if target == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                while left + 1 < right and nums[left] == nums[left + 1]:
+                    left = left + 1
+                while left < right - 1 and nums[right] == nums[right - 1]:
+                    right = right - 1
+                left, right = left + 1, right - 1
+            elif target < 0:
+                left = left + 1
+            else:
+                right = right - 1
+    return result
