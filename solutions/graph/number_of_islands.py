57888225d7bdd272a6e747f2eae35856fe38171f
Samiul Anik <samiul.anik@gmail.com>
2022-04-24 12:31:20 -0700

    number of island added

---
+++ solutions/graph/number_of_islands.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/number-of-islands/
+
+def numIslands(grid: list[list[str]]) -> int:
+    island_count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                island_count += dfs_num_of_islands(i, j, grid)
+    return island_count
+
+
+def dfs_num_of_islands(i: int, j: int, grid: list[list[str]]) -> int:
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
+        return 0
+    grid[i][j] = '0'
+    dfs_num_of_islands(i + 1, j, grid)
+    dfs_num_of_islands(i - 1, j, grid)
+    dfs_num_of_islands(i, j + 1, grid)
+    dfs_num_of_islands(i, j - 1, grid)
+    return 1
---
+++ tests/graph/number_of_islands_test.py
@@ -0,0 +1,21 @@
+from solutions.graph import number_of_islands
+
+
+def test_case_1():
+    grid = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+    ]
+    assert number_of_islands.numIslands(grid) == 1
+
+
+def test_case_2():
+    grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
+    ]
+    assert number_of_islands.numIslands(grid) == 3
