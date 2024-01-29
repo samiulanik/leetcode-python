7ebc87f4c56dd9db09375e8244f6551e383d65b4
Samiul Anik <samiulanik@users.noreply.github.com>
2024-01-28 16:56:38 -0800

    pre-commit updated

--- tests/adhoc/kth_factor_of_n_test.py
+++ tests/adhoc/kth_factor_of_n_test.py
@@ -5,8 +5,10 @@ from solutions.adhoc.kth_factor_of_n import kthFactor
 def test_case_1():
     assert kthFactor(12, 3) == 3
 
+
 def test_case_2():
     assert kthFactor(7, 2) == 7
+
 
 def test_case_3():
     assert kthFactor(4, 4) == -1
